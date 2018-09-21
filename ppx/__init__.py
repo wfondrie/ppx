afb68ab394aad494a05cdeb406dfdb5ed18030c3
wfondrie <wfondrie@umaryland.edu>
2018-09-21 11:57:55 -0700

    add import os.path

--- ppx/__init__.py
+++ ppx/__init__.py
@@ -1,13 +1,14 @@
 # __init__.py : ppx
+import os.path
 
 """
 See the README for detailed documentation and examples.
 """
 name = "ppx"
 
-from .PXDataset import PXDataset
-
 with open(os.path.join("..", "VERSION"), "r") as fh:
     version_num = fh.read().strip()
 
 __version__ = version_num
+
+from .PXDataset import PXDataset
