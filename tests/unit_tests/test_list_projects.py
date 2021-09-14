0e0e4025422851170efab3a7f452afa582c9b955
William Fondrie <fondriew@gmail.com>
2021-09-14 12:00:12 -0700

    Added fallback test

--- tests/unit_tests/test_list_projects.py
+++ tests/unit_tests/test_list_projects.py
@@ -1,6 +1,7 @@
 """Test that the list_projects() functions work"""
 import pytest
 import ppx
+import requests
 
 
 def test_pride():
@@ -15,3 +16,20 @@ def test_massive():
     proj = ppx.massive.list_projects()
     assert len(proj) > 10000
     assert all((p.startswith("MSV") or p.startswith("RMS")) for p in proj)
+
+
+def test_massive_fallback(no_requests):
+    """Test that we can get massive projects"""
+    proj = ppx.massive.list_projects()
+    assert len(proj) > 10000
+    assert all((p.startswith("MSV") or p.startswith("RMS")) for p in proj)
+
+
+@pytest.fixture
+def no_requests(monkeypatch):
+    """Remove requests.sessions.Session.request for all tests."""
+
+    def mock_get(*args, **kwargs):
+        raise OSError
+
+    monkeypatch.setattr(requests, "get", mock_get)
